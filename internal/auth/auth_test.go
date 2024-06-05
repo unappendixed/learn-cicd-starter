0bbba8d82a85d916a426412c9060959664547bc3
Unappendixed <38593737+unappendixed@users.noreply.github.com>
2024-06-05 16:01:39 -0700

    test: Add tests to internal/auth

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,39 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+
+	t.Run("fails with empty Authorization header", func(t *testing.T) {
+
+		headers := http.Header{
+			"Authorization": {""},
+		}
+		actual, actualerr := GetAPIKey(headers)
+
+        if actual != "" || actualerr == nil {
+            t.Errorf("expected error, got %s", actual)
+        }
+	})
+
+    t.Run("succeeds with valid api key", func(t *testing.T) {
+        headers := http.Header{
+            "Authorization": {"ApiKey 12345"},
+        }
+
+        expected := "12345"
+        actual, actualerr := GetAPIKey(headers)
+
+        if actualerr != nil {
+            t.Fatalf("unexpected error %s", actualerr.Error())
+        }
+
+        if actual != expected {
+            t.Errorf("expected %s got %s", expected, actual)
+        }
+    })
+
+}
